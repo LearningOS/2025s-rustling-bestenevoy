60fa97f688f61d60bdf6f48650bd1e13f387abe0
Bestenevoy <38575312+bestenevoy@users.noreply.github.com>
2025-03-25 10:08:06 +0000

    update

--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -3,21 +3,25 @@
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+// I AM DONE
 
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    ???
+    let a = input.to_string();
+    return a.trim().to_string();
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    let mut a = String::from(input);
+    a.push_str(" world!");
+    a
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    // 
+    input.to_string().replace("cars", "balloons")
 }
 
 #[cfg(test)]
